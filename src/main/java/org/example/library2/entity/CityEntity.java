7e439d7435b111b415ae526a84e10828a31a63ec
d1master1 <barancev2006@gmail.com>
2025-03-10 12:50:02 +0300

    City

--- src/main/java/org/example/library2/entity/CityEntity.java
+++ src/main/java/org/example/library2/entity/CityEntity.java
@@ -1,5 +1,50 @@
 package org.example.library2.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class CityEntity {
 
+    public CityEntity() {
+
+    }
+
+    public CityEntity(Long id, String title, List<PublisherEntity> publishers) {
+        this.id = id;
+        this.title = title;
+        this.publishers = publishers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<PublisherEntity> getPublishers() {
+        return publishers;
+    }
+
+    public void setPublishers(List<PublisherEntity> publishers) {
+        this.publishers = publishers;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    private List<PublisherEntity> publishers;
 }
